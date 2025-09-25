e01a56e193138309da62a89734e3b39850e58ca0
Youngjin <bryjna07@nate.com>
2025-09-25 16:34:47 +0900

    Feat: 폴더생성

--- KoreaSpots/Features/Home/HomeViewController.swift
+++ KoreaSpots/Features/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  KoreaSpots
 //
 //  Created by YoungJin on 9/25/25.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
