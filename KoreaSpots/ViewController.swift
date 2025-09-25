b58353ea158b40a1e1cd944ccb73a8cc6fef479e
Youngjin <bryjna07@nate.com>
2025-09-25 16:02:38 +0900

    Feat: 프로젝트 생성 #1

---
+++ KoreaSpots/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KoreaSpots
+//
+//  Created by YoungJin on 9/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
